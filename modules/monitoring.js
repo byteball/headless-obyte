c8d511af8953557d6f6d5c39100e128218c0811e
Papabyte <bencreze@yahoo.fr>
2019-04-03 06:11:04 +0200

    count lines depending of terminal width

--- modules/monitoring.js
+++ modules/monitoring.js
@@ -1,5 +1,6 @@
 var conf = require('ocore/conf.js');
 var bfirstPrint = true;
+var lineLengths = [];
 
 function start(){
 	printCompleteStatus();
@@ -8,36 +9,54 @@ function start(){
 
 function printCompleteStatus(){
 
+	if (!process.stdout.isTTY)
+		return;
+	var text;
+
 	if (conf.bLight){
 		var lightWallet =  require('ocore/light_wallet.js');
 
-		if (bfirstPrint)
+		if (bfirstPrint){
 			bfirstPrint = false;
+			process.stdout.write("\n");
+		}
 		else
 			process.stdout.moveCursor(0, -2);
 
 		printConnections();
-		process.stdout.write("\n" + (lightWallet.isFirstHistoryReceived() ? "First history received and processed" : "History not received yet"));
+		text = lightWallet.isFirstHistoryReceived() ? "First history received and processed" : "History not received yet";
+		process.stdout.write("\n");
+		process.stdout.write(text);
+		lineLengths.push(text.length)
 		process.stdout.clearLine(1);
 
 	} else {
 		var device = require('ocore/device.js');
 		var storage = require('ocore/storage.js');
-
 		return device.requestFromHub("get_last_mci", null, function(hubErr, last_hub_mci){
 
 			storage.readLastMainChainIndex(function(local_mci){
-				
-				if (bfirstPrint)
+				var linesCounter = 0;
+				if (bfirstPrint){
 					bfirstPrint = false;
-				else
-					process.stdout.moveCursor(0, -3)
-					
+					process.stdout.write("\n");
+				}else{
+					lineLengths.forEach(function(textLength){	//we have to count how many lines were used for the previous print to move the cursor backward accordingly
+						linesCounter+= Math.ceil(textLength/process.stdout.columns);
+					});
+					process.stdout.moveCursor(0, 1 - linesCounter);
+					lineLengths = [];
+				}
 				process.stdout.clearLine(0);
 				printConnections();
 				
 				if (!hubErr){
-					process.stdout.write("\nHub MCI: " + last_hub_mci + " - Local MCI: " + local_mci);
+					text = "Hub MCI: " + last_hub_mci + " - Local MCI: " + local_mci;
+					process.stdout.write("\n");
+					process.stdout.write(text);
+					lineLengths.push(text.length)
+
+					
 					process.stdout.clearLine(1);
 					var ratio = local_mci/last_hub_mci;
 					if (ratio > 1){
@@ -47,11 +66,21 @@ function printCompleteStatus(){
 					var fullBarLength = 50;
 					var completedLength = Math.floor(ratio * fullBarLength) + 1;
 					var uncompletedLength = fullBarLength - completedLength + 1;
-					process.stdout.write("\n" + (completedLength >= 1 ? Array(completedLength).join('=') : "") + (uncompletedLength >=1 ? Array(uncompletedLength).join('-') : "") + " " +  (100*ratio).toFixed(2) + "%");
+					process.stdout.write("\n" );
+					text = (completedLength >= 1 ? Array(completedLength).join('=') : "") + (uncompletedLength >=1 ? Array(uncompletedLength).join('-') : "") + " " +  (100*ratio).toFixed(2) + "%";
+					process.stdout.write(text);
+					lineLengths.push(text.length)
 				}else{
-					process.stdout.write("\nHub MCI: Unknown " + " - Local MCI: " + local_mci);
+					process.stdout.write("\n");
+					text = "Hub MCI: Unknown " + " - Local MCI: " + local_mci;
+					process.stdout.write(text);
+					lineLengths.push(text.length)
+
 					process.stdout.clearLine(1);
-					process.stdout.write("\nCouldn't get hub MCI level");
+					process.stdout.write("\n");
+					text = "Couldn't get hub MCI level";
+					process.stdout.write(text);
+					lineLengths.push(text.length)
 				}
 				process.stdout.clearLine(1);
 			});
@@ -66,8 +95,10 @@ function printConnections(){
 	var objConnectionStatus = network.getConnectionStatus();
 	process.stdout.cursorTo(0);
 	process.stdout.clearLine(0);
-	process.stdout.write("\n" + objConnectionStatus.incoming+" incoming connections, "+objConnectionStatus.outgoing+" outgoing connections, "+
-	objConnectionStatus.outgoing_being_opened+" outgoing connections being opened");
+	var text = objConnectionStatus.incoming+" incoming connections, "+objConnectionStatus.outgoing+" outgoing connections, "+
+	objConnectionStatus.outgoing_being_opened+" outgoing connections being opened";
+	process.stdout.write(text);
+	lineLengths.push(text.length);
 	process.stdout.clearLine(1);
 }
 

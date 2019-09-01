78da735eef93bf360a42e31d3632b51b7edea0aa
Tarmo <tarmo888@hotmail.com>
2019-09-01 16:34:51 +0300

    explorer for testnet

--- prosaic_contract_api.js
+++ prosaic_contract_api.js
@@ -108,8 +108,8 @@ function listenForPendingContracts(signWithLocalPrivateKey, callbacks) {
 						}
 						prosaic_contract.setField(contract.hash, "unit", unit);
 						device.sendMessageToDevice(contract.peer_device_address, "prosaic_contract_update", {hash: contract.hash, field: "unit", value: unit});
-						var url = 'https://explorer.obyte.org/#' + unit;
-						var text = "unit with contract hash for \""+ contract.title +"\" was posted into DAG " + url;
+						let explorer = (process.env.testnet ? 'https://testnetexplorer.obyte.org/#' : 'https://explorer.obyte.org/#');
+						var text = "unit with contract hash for \""+ contract.title +"\" was posted into DAG " + explorer + unit;
 						device.sendMessageToDevice(contract.peer_device_address, "text", text);
 
 						if (callbacks.onSigned)

bc2f1d4de946612aed3a15e348ff70a39ae440ab
Tarmo <tarmo888@hotmail.com>
2020-04-03 12:22:58 +0300

    Bind Creative chosen as witness

--- tools/replace-witnesses.js
+++ tools/replace-witnesses.js
@@ -8,6 +8,7 @@ if (!process.env.testnet) {
 	witnesses.push({'old': 'S7N5FE42F6ONPNDQLCF64E2MGFYKQR2I', 'new': 'FAB6TH7IRAVHDLK2AAWY5YBE6CEBUACF'}); // Fabien Marino
 	witnesses.push({'old': 'DJMMI5JYA5BWQYSXDPRZJVLW3UGL3GJS', 'new': '2TO6NYBGX3NF5QS24MQLFR7KXYAMCIE5'}); // Bosch Connectory Stuttgart
 	witnesses.push({'old': 'OYW2XTDKSNKGSEZ27LMGNOPJSYIXHBHC', 'new': 'APABTE2IBKOIHLS2UNK6SAR4T5WRGH2J'}); // PolloPollo
+	witnesses.push({'old': 'BVVJ2K7ENPZZ3VYZFWQWK7ISPCATFIW3', 'new': 'DXYWHSZ72ZDNDZ7WYZXKWBBH425C6WZN'}); // Bind Creative
 }
 
 witnesses.forEach(function(replacement) {

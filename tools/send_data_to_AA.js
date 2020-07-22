bbb66af512a5452c373448427a9cb12640be9b96
Tarmo <tarmo888@hotmail.com>
2020-07-22 15:30:09 +0300

    comments

--- tools/send_data_to_AA.js
+++ tools/send_data_to_AA.js
@@ -6,24 +6,29 @@ const eventBus = require('ocore/event_bus.js');
 function sendDataToAA(){
 	headlessWallet.readFirstAddress((first_address) => {
 		let payload = {};
-		//payload.d = {xx: 66.3,sub: 22.1};
-		//payload.sub = 22.1;
-		//payload.output = {address: first_address};
+		// different ways how this specific AA accepts data, enable all or disable all to see different results
+
+		//payload.d = {xx: 66.3,sub: 22.1}; // this results the same as below because the AA definition
+		payload.sub = 22.1;
+
+		//payload.output = {address: first_address}; // this results the same as below because the AA definition
 		payload.payment = {
-			asset: "base",
+			asset: "base", // base asset is bytes
 			outputs: [
-				{address: first_address},
+				{address: first_address}, // if output has only address and no amount, all from this asset is sent
 			]
 		};
+
 		let opts = {
-			paying_addresses: [first_address],
-			change_address: first_address,
+			paying_addresses: [first_address], // first address pays the fees
+			change_address: first_address, // and first address gets back the chance
 			messages: [
 				{app: "data", payload},
 			],
-			to_address: "24WUKC3BDXCDUNKEZE52IT77S66OFW3L",
-			amount: 10000
+			to_address: "24WUKC3BDXCDUNKEZE52IT77S66OFW3L", // AA address
+			amount: 10000 // minimal fee for AA
 		};
+		// this is a multi-purpose sending function, which can send payments and data
 		headlessWallet.sendMultiPayment(opts, (err, unit) => {
 			if (err) {
 				console.error(err);
@@ -35,4 +40,5 @@ function sendDataToAA(){
 	});
 }
 
+// we wait for the wallet to get ready and then execute this function
 eventBus.on('headless_wallet_ready', sendDataToAA);
